7ba7081869b541e8b39994a9bf824fdf74dc1223
sunqi <524920679@qq.com>
2017-06-16 13:37:16 +0800

    修复bug & 注释补充

--- src/test/Auto.java
+++ src/test/Auto.java
@@ -10,26 +10,37 @@ import jmidi.*;
 public class Auto {
 
 	public static void main(String[] args) throws Exception {
-		Play play = new Play(2);
+		Play play = new Play(2); // 设置音色
 
-		int section = 1, prev = 9, type = rand(Rhythm.data.length);
-		int[] rhythm = Rhythm.get(type), path = Path.get(rand(Path.data.length)); // 节奏型和走向
+		int section = 1; // 当前第几小节
+		int prev = 9; // 上一个音符，初始化设置为9
+		int range = 15; // 随机生成的音符范围
+		int[] rhythm = Rhythm.get(rand(Rhythm.data.length)); // 随机选择节奏型
+		int[] path = Path.get(rand(Path.data.length)); // 随机选择走向
 
 		while (true) {
 			for (int i = 0, chd = 0; i < rhythm.length; i++) {
-				for (int count = 0, key = rand(15); count < 3 || section == 1; count++) {
-					if (chk(key, prev, path[section - 1])) {
-						int range = (prev = key) / 6, melody = melody(key); // 旋律
-
-						play.melody(Note.key(range, melody));
-						break;
+				// 旋律区
+				{
+					for (int count = 0, key = rand(range); count < 3 || section == 1; count++) {
+						if (chk(key, prev, path[section - 1])) {
+							int area = (prev = key) / 6; // 转换成区域
+							int melody = melody(key); // 转换成音符
+							
+							play.melody(Note.key(area, melody)); // 播放旋律
+							break;
+						}
+						key = rand(range); // 生成的音符不符合规范，重新生成，有3次机会，如果是第一小节，必须全部生成
 					}
-					key = rand(15); // 生成的音符不符合规范，重新生成，有3次机会，如果是第一小节，必须全部生成
 				}
-				if (rhythm[i] == 1) {
-					int[][] chords = Chord.get(path[section - 1]); // 和弦
-					int[] chord = chords[chd++ % chords.length];
-					play.chord(Note.key(chord[0], chord[1]));
+				// 和弦区
+				{
+					if (rhythm[i] == 1) {
+						int[][] chords = Chord.get(path[section - 1]);
+						int[] chord = chords[chd++ % chords.length];
+						
+						play.chord(Note.key(chord[0], chord[1])); // 播放和弦
+					}
 				}
 				Thread.sleep(260);
 			}
@@ -38,7 +49,7 @@ public class Auto {
 	}
 
 	private static int melody(int key) {
-		return (key %= 6) > 3 ? key + 2 : key + 1;
+		return (key %= 6) > 2 ? key + 2 : key + 1;
 	}
 
 	private static boolean chk(int key, int prev, int path) {
