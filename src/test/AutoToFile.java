0b4022fef00ae7b4559ee2be740a439845a4b916
sunqi <524920679@qq.com>
2017-06-20 14:19:26 +0800

    修复bug

--- src/test/AutoToFile.java
+++ src/test/AutoToFile.java
@@ -12,18 +12,19 @@ import jmidi.*;
  */
 public class AutoToFile {
 
+	private static final int range = 15; // 随机生成的音符范围
+	private static final int[] rhythm = Rhythm.rand(); // 随机选择节奏型
+	private static final int[] path = Path.rand(); // 随机选择走向
+
+	private static final int bpm = 120; // 速度，类库要求，不清楚具体用途
+	private static final int velocity = 30; // 这个参数决定速度，不能大于bpm
+	private static final int max = 64; // 生成多少小节，最好是8的倍数
+
+	private static int section = 1; // 当前第几小节，用于选择走向
+	private static int prev = 9; // 上一个音符，初始化设置为9
+
 	public static void main(String... args) throws Exception {
 		File file = new File("e://test.mid");
-
-		int section = 1; // 当前第几小节，用于选择走向
-		int prev = 9; // 上一个音符，初始化设置为9
-		int range = 15; // 随机生成的音符范围
-		int[] rhythm = Rhythm.rand(); // 随机选择节奏型
-		int[] path = Path.rand(); // 随机选择走向
-
-		int bpm = 120; // 速度，类库要求，不清楚具体用途
-		int velocity = 30; // 这个参数决定速度，不能大于bpm
-		int max = 64; // 生成多少小节，最好是8的倍数
 
 		Sequence seq = new Sequence();
 		Track main = seq.createTrack();
@@ -32,21 +33,21 @@ public class AutoToFile {
 		Track trackMelody = seq.createTrack();
 		Track trackChord = seq.createTrack();
 
-		prev = must(trackMelody, range, prev, path[section - 1], velocity);
-		prev = must(trackMelody, range, prev, path[section - 1], bpm);
+		prev = must(trackMelody, 0);
+		prev = must(trackMelody, bpm - velocity);
 		
-		int index = 0;
+		int cur = 0;
 		do {
 			for (int i = 0, chd = 0; i < rhythm.length; i++) {
-				int pos = (bpm - velocity) * ((index * rhythm.length + i) + 2) + velocity; // 计算音符时间
+				int pos = (bpm - velocity) * ((cur * rhythm.length + i) + 2); // 计算音符时间
 				
 				// 旋律区
 				{
-					if(section == path.length){
-						if(i > 0 && Note.melody(prev) != Chord.get(path[section - 1])[0][1])
-							prev = must(trackMelody, range, prev, path[section - 1], pos);
+					if(section == path.length){ // 每次走向的最后一小节
+						if(i > 0 && Note.melody(prev) != path[section - 1])
+							prev = must(trackMelody, pos); // 直到生成和弦根音为止
 					}else{
-						prev = must(trackMelody, range, prev, path[section - 1], pos, 3);
+						prev = must(trackMelody, pos, 3);
 					}
 				}
 				// 和弦区
@@ -60,7 +61,7 @@ public class AutoToFile {
 				}
 			}
 			section = (section == path.length) ? 1 : section + 1;
-		} while (index++ < max);
+		} while (++cur < max);
 
 		FileOutputStream out = new FileOutputStream(file);
 		MidiFileWriter.write(seq, out);
@@ -68,36 +69,40 @@ public class AutoToFile {
 		new Play(file);
 	}
 	
-	private static boolean chk(int key, int prev, int path) {
-		return key - prev < 5 && key - prev > -5 && key != prev && Melody.get(path, Note.melody(key));
+	private static boolean chk(int key, int root) {
+		if (key - prev > 5)
+			return false;
+		if (prev - key > 5)
+			return false;
+		if (key == prev)
+			return false;
+
+		return Melody.get(root, Note.melody(key));
 	}
 	
 	/**
 	 * 生成音符
 	 * 
 	 * @param track 音轨
-	 * @param range 区域
-	 * @param prev 上一个key
-	 * @param path 根音
 	 * @param pos 时间轴上的位置
 	 * @param count 重新生成次数，如果不传，一直生成，直到合法为止
 	 * @return
 	 * @throws Exception
 	 */
-	private static int must(Track track, int range, int prev, int path, int pos, int... count) throws Exception {
+	private static int must(Track track, int pos, int... count) throws Exception {
 		int i = 0;
 		int max = (count.length > 0) ? count[0] : 0;
 		
 		do {
 			int key = Note.rand(range);
-			if (chk(key, prev, path)) {
+			if (chk(key, path[section - 1])) {
 				int area = key / 6; // 转换成区域
 				int melody = Note.melody(key); // 转换成音符
 
 				track.add(Note.key(area, melody), pos, 127);
 				return key;
 			}
-		} while (i++ < max || count.length == 0);
+		} while (++i < max || count.length == 0);
 		
 		return prev;
 	}
